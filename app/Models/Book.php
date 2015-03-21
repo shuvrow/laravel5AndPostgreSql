0c58b1e49d809a5e6420229f9b8eb2e22383b4c1
shuvrow <shuvrow@gmail.com>
2015-03-21 01:23:15 -0400

    Laravel 5 with PostGreSql

---
+++ app/Models/Book.php
@@ -0,0 +1,10 @@
+<?php namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Book extends Eloquent {
+
+	protected $table='books';
+   // protected $primaryKey='book_id';
+
+}
